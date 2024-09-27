a6a7f0d6ba8c3d7293c161b1c68a10847dd24fce
ohyeahx1 <90593572+ohyeahx1@users.noreply.github.com>
2024-09-27 08:48:16 +0700

    Update ohyeahx5.py

--- ohyeahx5.py
+++ ohyeahx5.py
@@ -1,1 +1,15 @@
-import numpy
+BLUE = "\33[94m"
+RESET = "\033[0m"
+banner = f"""{BLUE}                                                       
+From:                   88                                                
+                        88                ,d                              
+                        88                88                              
+,adPPYba,   ,adPPYba,   88   ,adPPYba,  MM88MMM  ,adPPYba,   8b,dPPYba,   
+I8[    ""  a8"     "8a  88  a8"     "8a   88    a8"     "8a  88P'    "8a  
+ `"Y8ba,   8b       d8  88  8b       d8   88    8b       d8  88       d8  
+aa    ]8I  "8a,   ,a8"  88  "8a,   ,a8"   88,   "8a,   ,a8"  88b,   ,a8"  
+`"YbbdP"'   `"YbbdP"'   88   `"YbbdP"'    "Y888  `"YbbdP"'   88`YbbdP"'   
+                                                             88           
+                                                             88         with love ❤️{RESET}"""
+
+print(banner)
